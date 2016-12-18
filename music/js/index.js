8b4069aed0df3fc430336f6349808ecd3d6c9e41
cuiyuxin <914850097@qq.com>
2016-12-18 15:34:48 +0800

    music

--- music/js/index.js
+++ music/js/index.js
@@ -15,11 +15,14 @@ window.onload=function(){
 }
 $(function(){
     $(".main-title span").eq(0).css({"color":"#fff","background":"#DB525A","border-radius":"50px"});
-
+    $(".news").css("display","none");
+    $(".news").eq(0).css("display","block");
     $(".main-title span").click(function(){
        var index=$(this).index(".main-title span");
         $(".main-title span").css({"color":"","background":"","border-radius":""});
         $(".main-title span").eq(index).css({"color":"#fff","background":"#DB525A","border-radius":"50px"});
+        $(".news").css("display","none");
+        $(".news").eq(index).css("display","block");
     })
 
     $(".menu-list a").eq(0).css({"color":"#fff","background":"#DB525A","border-radius":"40px"});
