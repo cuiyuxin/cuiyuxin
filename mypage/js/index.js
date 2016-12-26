a6b26dd291d23af6ffe87eaeb79c1cbdb3f2c1c2
cuiyuxin <914850097@qq.com>
2016-12-26 15:47:05 +0800

    mypage

--- mypage/js/index.js
+++ mypage/js/index.js
@@ -1,21 +1,24 @@
 $(function(){
-    $(".s-title").click(function(){
+    $(".small-nav").click(function(){
         $(".s-con").slideToggle(200);
     })
+    $(".screen li").css("display","none");
+    $(".screen li").eq(0).css("display","block");
 
     $(".nav li").click(function(){
         $(".nav li").css("background","rgba(255,255,255,.4)");
         $(".nav li a").css("color","#000");
         $(this).css("background","rgba(255,255,255,.6)");
-        $(".screen li").css("display","none");
+        $(".screen li").fadeOut();
         var index=$(this).index(".nav li");
         $(".screen li").eq(index).fadeIn();
         $(".nav li a").eq(index).css("color","#5394D6")
     })
 
-    $(".small-nav li").click(function(){
-        $(".screen li").css("display","none");
-        var index=$(this).index(".small-nav li");
+
+    $(".s-con li").click(function(){
+        $(".screen li").fadeOut();
+        var index=$(this).index(".s-con li");
         $(".screen li").eq(index).fadeIn();
     })
 })
